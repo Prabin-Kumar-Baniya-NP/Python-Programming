8dd22d5cf615dbd357958b5e7f88c8195e064a59
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2020-04-03 19:40:36 +0530

    OOPs Concept Examples

--- 13.OOP's-1.py
+++ 13.OOP's-1.py
@@ -40,4 +40,19 @@ ram = information("Ram", "Delhi", 15)
 shyam = information("Shyam", "Kathmandu", 16)
 print(ram.get_info())
 print(shyam.get_info())
+
+
 # **********************************************************************************************************************
+# Example 3
+class Compute:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def operation(self):
+        return (self.x + self.y + self.z, self.x - self.y - self.z)
+
+
+digits = Compute(3, 2, 1)
+print(digits.operation())
